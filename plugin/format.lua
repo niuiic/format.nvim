c04c38b7c05acd950a9bd6a621e6d7d393399b50
niuiic <niuiicde@gmail.com>
2024-11-06 08:52:46 +0800

    fix: format on save

--- plugin/format.lua
+++ plugin/format.lua
@@ -3,19 +3,20 @@ local bufs = {}
 vim.api.nvim_create_autocmd("BufWritePost", {
 	callback = function(args)
 		local config = require("format")._config:get()
-		if config.format_on_save and config.filetypes[vim.bo.filetype] then
-			if bufs[args.buf] then
-				bufs[args.buf] = nil
-				return
-			end
-			require("format").format(function()
-				local ok = pcall(function()
-					vim.cmd("silent w")
-				end)
-				if ok then
-					bufs[args.buf] = true
-				end
-			end)
+		if not config.format_on_save or not config.filetypes[vim.bo.filetype] then
+			return
 		end
+
+		if bufs[args.buf] then
+			bufs[args.buf] = false
+			return
+		end
+
+		bufs[args.buf] = true
+		require("format").format(function()
+			pcall(function()
+				vim.cmd("silent w")
+			end)
+		end)
 	end,
 })
