4bdfccc48c5a423b5ddb73682b7839878b260155
niuiic <niuiicde@gmail.com>
2024-11-18 23:06:13 +0800

    refactor: format on save

--- plugin/format.lua
+++ plugin/format.lua
@@ -1,21 +1,13 @@
-local bufs = {}
-
 vim.api.nvim_create_autocmd("BufWritePost", {
-	callback = function(args)
+	callback = function()
 		local config = require("format")._config:get()
 		if not config.format_on_save or not config.filetypes[vim.bo.filetype] then
 			return
 		end
 
-		if bufs[args.buf] then
-			bufs[args.buf] = false
-			return
-		end
-
-		bufs[args.buf] = true
 		require("format").format(function()
 			pcall(function()
-				vim.cmd("silent w")
+				vim.cmd("silent! w")
 			end)
 		end)
 	end,
